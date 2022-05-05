e89fde2e61c9faff750b783c5ee4c4ada88aa2aa
Anas <104628733+RengerAnas@users.noreply.github.com>
2022-05-05 15:05:16 +0530

    this is student grade project

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+
+const calcy = ()=>{
+let a = document.getElementById('math').value;
+let b = document.getElementById('phy').value;
+let c = document.getElementById('webpro').value;
+let d = document.getElementById('science').value;
+
+
+let total =  parseFloat(a)+parseFloat(b)+parseFloat(c)+parseFloat(d) ;
+
+let perc = (total/400) * 100;
+
+let grade = "";
+if (perc<=100 && perc>=80) {
+    grade = "your Grade is : A";
+} else if(perc>=60 && perc<80) {
+    grade = "your Grade is : B";
+} else if (perc>=40 && perc<60){
+    grade = "your Grade is : C";
+} else if (perc>=33 && perc<40){
+    grade = "your Grade is : E";
+}else {
+    grade = "you are fail you percentage is less than 33 !!!"
+}
+if(perc>100){
+    document.getElementById('result').innerHTML = "please Enter the currect marks"
+} else {
+document.getElementById('result').innerHTML = `Your total marks are ${total} , your percentage is ${perc} <br> and ${grade}` ;
+}
+// alert(perc);
+}
